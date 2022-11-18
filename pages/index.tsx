dbaefcb263352f2562457d4e33b378ffbb75cc35
hparsa <hparsaa1985@gmail.com>
2022-11-17 21:41:07 -0500

    Adding feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Sidebar from "../components/Sidebar";
+import Feed from "../components/Feed";
 
 export default function Home() {
   return (
@@ -13,6 +14,8 @@ export default function Home() {
       <main className="flex min-h-screen max-w-7xl mx-auto ">
         {/*Sidebar*/}
         <Sidebar/>
+         {/* Feed */}
+        <Feed />
       
         </main>
     </div>
