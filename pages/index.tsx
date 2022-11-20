3986be92de64cb182f49c95bad891456eb6e0575
KrakenZim <104575615+KrakenZim@users.noreply.github.com>
2022-11-20 14:49:38 -0500

    Search bar and Widgets Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Sidebar from "../components/Sidebar";
 import Feed from "../components/Feed";
+import Widgets from "../components/Widgets";
 
 export default function Home() {
   return (
@@ -16,6 +17,8 @@ export default function Home() {
         <Sidebar/>
          {/* Feed */}
         <Feed />
+        {/* Widget */}
+        <Widgets/>
       
         </main>
     </div>
