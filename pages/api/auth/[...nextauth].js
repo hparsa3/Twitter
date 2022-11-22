55270a03dee22566888d40244c1c5d19b8a43a5d
hparsa <hparsaa1985@gmail.com>
2022-11-22 15:02:40 -0500

    20221122

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,39 +1,27 @@
-import NextAuth from "next-auth"
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
-import GithubProvider from "next-auth/providers/github"
-
-
-
-export const authOptions = {
-
+export default NextAuth({
   // Configure one or more authentication providers
-
   providers: [
-
-    GithubProvider({
-
-      clientId: process.env.GITHUB_ID,
-
-      clientSecret: process.env.GITHUB_SECRET,
-
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-
     // ...add more providers here
-
   ],
+  pages: {
+    signIn: "/auth/signin",
+  },
 
- 
-
-  pages:{
-
-    signIn:"/auth/signin",
-
-   
-
-  }
-
-}
-
-
-
-export default NextAuth(authOptions)
+  callbacks: {
+    async session({ session, token }) {
+      session.user.username = session.user.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
+});
