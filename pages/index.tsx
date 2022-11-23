4e8435997d12301a60f8caee92d7256fcdccc227
Amir <amirsharifico@gmail.com>
2022-11-23 14:13:57 -0500

    update widgit

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,25 +49,4 @@ export async function getServerSideProps() {
     },
   };
 }
-     
 
-    
-    </div>
-  );
-}
-
-
-export async function getServerSideProps() {
-  const newsResults = await fetch(
-    "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
-   
-  ).then((res) => res.json());
-  const randomUsersResults = await fetch("https://randomuser.me/api/?results=30&inc=name,login,picture"
-
-  ).then((res) => res.json());
-  return {
-    props: {
-      newsResults,
-      randomUsersResults,
-    }
-  }}
