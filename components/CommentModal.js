9896b2a6c4eff5683853786067e0ca6b27bede1f
Amir <amirsharifico@gmail.com>
2022-11-30 17:03:39 -0500

    update CommentModal 30-Nov-2022

--- components/CommentModal.js
+++ components/CommentModal.js
@@ -1,134 +1,138 @@
+import { useRecoilState } from "recoil";
+import { modalState, postIdState } from "../atom/modalAtom";
+import { useRouter } from "next/router";
+import Modal from "react-modal";
+
 import {
-    ChartBarIcon,
-    ChatIcon,
-    DotsHorizontalIcon,
-    HeartIcon,
-    ShareIcon,
-    TrashIcon,
-  } from "@heroicons/react/outline";
-  import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
-  import Moment from "react-moment";
-  import {
-    collection,
-    deleteDoc,
-    doc,
-    onSnapshot,
-    setDoc,
-  } from "firebase/firestore";
-  import { db, storage } from "../firebase";
-  import { signIn, useSession } from "next-auth/react";
-  import { useState, useEffect } from "react";
-  import { deleteObject, ref } from "firebase/storage";
-  import { useRecoilState } from "recoil";
-  import { modalState } from "../atom/modalAtom";
+  EmojiHappyIcon,
+  PhotographIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import {
+  addDoc,
+  collection,
+  doc,
+  onSnapshot,
+  serverTimestamp,
+} from "firebase/firestore";
+import Moment from "react-moment";
+import { userState  } from "next-auth/react";
+
+export default function CommentModal() {
+  const [open, setOpen] = useRecoilState(modalState);
+  const [postId] = useRecoilState(postIdState);
+  const [currentUser] = useRecoilState(userState);
+  const [post, setPost] = useState({});
+  const [input, setInput] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    onSnapshot(doc(db, "posts", postId), (snapshot) => {
+      setPost(snapshot);
+    });
+  }, [postId, db]);
+  //********************************************************************************* */
+  async function sendComment() {
+    await addDoc(collection(db, "posts", postId, "comments"), {
+      comment: input,
+      name: currentUser.name,
+      username: currentUser.username,
+      userImg: currentUser.userImg,
+      timestamp: serverTimestamp(),
+      userId: currentUser.uid,
+    });
   
-  export default function Post({ post }) {
-    const { data: session } = useSession();
-    const [likes, setLikes] = useState([]);
-    const [hasLiked, setHasLiked] = useState(false);
-    const [open, setOpen] = useRecoilState(modalState)
-  
-    useEffect(() => {
-      const unsubscribe = onSnapshot(
-        collection(db, "posts", post.id, "likes"),
-        (snapshot) => setLikes(snapshot.docs)
-      );
-    }, [db]);
-    useEffect(() => {
-      setHasLiked(
-        likes.findIndex((like) => like.id === session?.user.uid) !== -1
-      );
-    }, [likes]);
-    async function likePost() {
-      if (session) {
-        if (hasLiked) {
-          await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid));
-        } else {
-          await setDoc(doc(db, "posts", post.id, "likes", session?.user.uid), {
-            username: session.user.username,
-          });
-        }
-      } else {
-        signIn();
-      }
-    }
-    async function deletePost() {
-      if (window.confirm("Are you sure you want to delete this post?")) {
-        deleteDoc(doc(db, "posts", post.id));
-        if (post.data().image) {
-          deleteObject(ref(storage, `posts/${post.id}/image`));
-        }
-      }
-    }
-    return (
-      <div className="flex p-3 cursor-pointer border-b border-gray-200">
-        {/* user image */}
-        <img
-          className="h-11 w-11 rounded-full mr-4"
-          src={post.data().userImg}
-          alt="user-img"
-        />
-        {/* right side */}
-        <div className="">
-          {/* Header */}
-          <div className="flex items-center justify-between">
-            {/* post user info */}
-            <div className="flex items-center space-x-1 whitespace-nowrap">
+    setOpen(false);
+    setInput("");
+    router.push(`/posts/${postId}`);
+  }
+  //********************************************************************************* */
+  return (
+    <div>
+      {open && (
+        <Modal
+          isOpen={open}
+          onRequestClose={() => setOpen(false)}
+          className="max-w-lg w-[90%]  absolute top-24 left-[50%] translate-x-[-50%] bg-white border-2 border-gray-200 rounded-xl shadow-md"
+        >
+          <div className="p-1">
+            <div className="border-b border-gray-200 py-2 px-1.5">
+              <div
+                onClick={() => setOpen(false)}
+                className="hoverEffect w-10 h-10 flex items-center justify-center"
+              >
+                <XIcon className="h-[23px] text-gray-700 p-0" />
+              </div>
+            </div>
+            <div className="p-2 flex items-center space-x-1 relative">
+              <span className="w-0.5 h-full z-[-1] absolute left-8 top-11 bg-gray-300" />
+              <img
+                className="h-11 w-11 rounded-full mr-4"
+                src={post?.data()?.userImg}
+                alt="user-img"
+              />
               <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
-                {post.data().name}
+                {post?.data()?.name}
               </h4>
               <span className="text-sm sm:text-[15px]">
-                @{post.data().username} -{" "}
+                @{post?.data()?.username} -{" "}
               </span>
               <span className="text-sm sm:text-[15px] hover:underline">
-                <Moment fromNow>{post?.data().timestamp?.toDate()}</Moment>
+                <Moment fromNow>{post?.data()?.timestamp?.toDate()}</Moment>
               </span>
             </div>
-            {/* dot icon */}
-            <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2" />
-          </div>
-          {/* post text */}
-          <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
-            {post.data().text}
-          </p>
-          {/* post image */}
-          <img className="rounded-2xl mr-2" src={post.data().image} alt="" />
-          {/* icons */}
-  
-          <div className="flex justify-between text-gray-500 p-2">
-           
-            <ChatIcon onClick={()=>setOpen(!open)} className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
-            {session?.user.uid === post?.data().id && (
-              <TrashIcon
-                onClick={deletePost}
-                className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+            <p className="text-gray-500 text-[15px] sm:text-[16px] ml-16 mb-2">
+              {post?.data()?.text}
+            </p>
+
+            <div className="flex  p-3 space-x-3">
+              <img
+                src={currentUser.userImg}
+                alt="user-img"
+                className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
               />
-            )}
-            <div className="flex items-center">
-              {hasLiked ? (
-                <HeartIconFilled
-                  onClick={likePost}
-                  className="h-9 w-9 hoverEffect p-2 text-red-600 hover:bg-red-100"
-                />
-              ) : (
-                <HeartIcon
-                  onClick={likePost}
-                  className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
-                />
-              )}
-              {likes.length > 0 && (
-                <span
-                  className={`${hasLiked && "text-red-600"} text-sm select-none`}
-                >
-                  {" "}
-                  {likes.length}
-                </span>
-              )}
+              <div className="w-full divide-y divide-gray-200">
+                <div className="">
+                  <textarea
+                    className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
+                    rows="2"
+                    placeholder="Tweet your reply"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                  ></textarea>
+                </div>
+
+                <div className="flex items-center justify-between pt-2.5">
+                  <div className="flex">
+                    <div
+                      className=""
+                      // onClick={() => filePickerRef.current.click()}
+                    >
+                      <PhotographIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                      {/* <input
+                        type="file"
+                        hidden
+                        ref={filePickerRef}
+                        onChange={addImageToPost}
+                      /> */}
+                    </div>
+                    <EmojiHappyIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                  </div>
+                  <button
+                    onClick={sendComment}
+                    disabled={!input.trim()}
+                    className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+                  >
+                    Reply
+                  </button>
+                </div>
+              </div>
             </div>
-            <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
-            <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           </div>
-        </div>
-      </div>
-    );
-  }
+        </Modal>
+      )}
+    </div>
+  );
+}
